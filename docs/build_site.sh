fdcc67abe6d5c38c663e81a8bfa89a99d2fa9ceb
liao <liaomovie2@gmail.com>
2019-04-08 00:11:01 +0800

    up

--- docs/build_site.sh
+++ docs/build_site.sh
@@ -2,8 +2,7 @@ olddir=`pwd`
 cd ~/Desktop/Rmd_talk/docs
 
 [[ -d slide ]] || mkdir slide
-cp -r ../img slide
-cp -r ../index_files slide
+cp -r ../img ../addons ../index_files slide
 cp ../index.html slide
 
 Rscript build_idx.R
