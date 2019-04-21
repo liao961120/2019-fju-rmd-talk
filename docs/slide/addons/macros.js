7483b09aca09efd802840e7dbe27d847392bad5f
liao <liaomovie2@gmail.com>
2019-04-21 14:16:21 +0800

    up

--- docs/slide/addons/macros.js
+++ docs/slide/addons/macros.js
@@ -12,3 +12,8 @@ remark.macros['gen'] = function(w, cl="") {
   var url = this;
   return '<img src="' + url + '" style="width: ' + w + '" class="' + cl + '" />';
 };
+
+remark.macros['abs'] = function(width="30%", left="85%", top="15%", cl="") {
+  var url = this;
+  return '<img src="' + url + '" style="position:absolute;left:' + left + ';top:' + top + ';width:' + width + '" class="' + cl + '" />';
+};
